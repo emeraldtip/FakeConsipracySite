ac76ba371c2374d2ce0f02a63059639afb8b5474
unknown <emeraldtipyt@gmail.com>
2023-03-14 19:32:10 +0200

    Trying to get headlines working in navbar

--- application.py
+++ application.py
@@ -1,18 +1,34 @@
 from flask import *
-
+import os
 app = Flask(__name__)
 
+#every article is within the news folder in another folder, with the article numbers counting up in order of priority (so #1 is most important, #2 second place etc.)
 #ok markdown we use markdown to do bold italic and bolditalic
-#and then png file is the cover image
-#jpg file is the author image
+#and then png file is the cover image - so find every png image in folder and pick first of em
+#jpg file is the author image - same as the png process
 #This is very nicely and well structured and totally not flawed and can totally be expanded as I very much care about what happens to this site after the short project is done
-
-@app.route("/")
+def getHeadlines():
+    headlines = []
+    for item in os.listdir("data/news"):
+        path = ""
+        if item.startswith("article"):
+            for i in "data/news/"+item:
+                if i.endswith(".txt"):
+                    with open(i,"w") as file:
+                        headlines.append(file.readline())
+    return headlines
+@app.route("/") #homepage with a few big news stories
 def home():
-    return render_template("default.html")
-@app.route("/uudised")
+    return render_template("default.html",headlines=getHeadlines())
+
+@app.route("/uudised") #list of all the news stories
 def news():
-    return render_template("uudised.html")
+    return render_template("uudised.html",headlines=getHeadlines())
+    
+@app.route("/uudised/") #generates the page for a news story on the spot
+def article():
+    #something like the delfi article website, except without the date and actually most of the stuff
+    return render_template("article.html",headlines=getHeadlines())
 
 if __name__ == "__main__":
     app.run()
