b5349ad10c34f986a9dede219856fe9fcab6b23f
emeraldtip <emeraldtipyt@gmail.com>
2023-03-16 22:53:50 +0200

    added favicon

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -18,17 +18,15 @@ setColor();
 
 function truncate(height) {
   let descs = document.getElementsByClassName("desc");
-  
   function getHeight(elt) { return elt.getBoundingClientRect().height; }
   function shorten(str)   { return str.slice(0, -1); }
-  
+    
   for (let i = 0; i<descs.length; i++)
   {
-      elt = descs[i];
-      content = elt.textContent;
-      while (getHeight(elt) > height && content) {
-        elt.textContent = (content = shorten(content)) + '...';
-      }
-  }
-  
+    elt = descs[i];
+    content = elt.textContent;
+    while (getHeight(elt) > height && content) {
+      elt.textContent = (content = shorten(content)) + '...';
+    }
+  } 
 }
